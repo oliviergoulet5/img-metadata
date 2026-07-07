c470d65a4e07121e38536d6041254db7987c87ab
Olivier Goulet <olivier_goulet@icloud.com>
2026-07-07 12:59:38 -0400

    feat: remove debug info

--- src/main.cpp
+++ src/main.cpp
@@ -40,12 +40,8 @@ auto main(int argc, char* argv[]) -> int {
 
         auto metadata = ret.value();
 
-        std::println("Size: {} bytes", bytes->size());
-        std::println("First bytes: {:02X} {:02X} ...",
-            std::to_integer<unsigned>(view[0]),
-            std::to_integer<unsigned>(view[1])
-        );
         std::println("Format: {}", image_format_to_string(metadata.format));
+        std::println("Size: {} bytes", bytes->size());
         std::println("Width: {}", metadata.dimensions.width);
         std::println("Height: {}", metadata.dimensions.height);
         std::println("Bit Depth: {}", metadata.bit_depth);
