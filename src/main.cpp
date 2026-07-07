cb0e2ec2baf36f4a8c0a8f55fdd51a62d5c8da59
Olivier Goulet <olivier_goulet@icloud.com>
2026-07-06 23:25:39 -0400

    feat: support multiple inputs

--- src/main.cpp
+++ src/main.cpp
@@ -16,30 +16,44 @@ auto main(int argc, char* argv[]) -> int {
         return EXIT_FAILURE;
     }
 
-    std::string_view file = argv[1];
-    std::println("File: {}", file);
+    int exit_code = 0;
 
-    // Read image file and retrieve bytes.
-    auto bytes = read_file(argv[1]);
-    if (!bytes) return EXIT_FAILURE;
+    for (int i = 0; i < argc; ++i) {
+        std::string_view file = argv[i];
+        std::println("File: {}", file);
 
-    // Display size of image file and first couple of bytes.
-    auto view = std::span{*bytes};
+        // Read image file and retrieve bytes.
+        auto bytes = read_file(file);
+        if (!bytes) {
+            exit_code = EXIT_FAILURE;
+            continue;
+        };
 
-    auto ret = get_image_metadata(view);
-    if (!ret) return EXIT_FAILURE;
+        // Display size of image file and first couple of bytes.
+        auto view = std::span{*bytes};
 
-    auto metadata = ret.value();
+        auto ret = get_image_metadata(view);
+        if (!ret) {
+            exit_code = EXIT_FAILURE;
+            continue;
+        };
 
-    std::println("Size: {} bytes", bytes->size());
-    std::println("First bytes: {:02X} {:02X} ...",
-        std::to_integer<unsigned>(view[0]),
-        std::to_integer<unsigned>(view[1])
-    );
-    std::println("Format: {}", image_format_to_string(metadata.format));
-    std::println("Width: {}", metadata.dimensions.width);
-    std::println("Height: {}", metadata.dimensions.height);
-    std::println("Bit Depth: {}", metadata.bit_depth);
+        auto metadata = ret.value();
 
+        std::println("Size: {} bytes", bytes->size());
+        std::println("First bytes: {:02X} {:02X} ...",
+            std::to_integer<unsigned>(view[0]),
+            std::to_integer<unsigned>(view[1])
+        );
+        std::println("Format: {}", image_format_to_string(metadata.format));
+        std::println("Width: {}", metadata.dimensions.width);
+        std::println("Height: {}", metadata.dimensions.height);
+        std::println("Bit Depth: {}", metadata.bit_depth);
+
+        if (argc > i + 1) {
+            std::println("---");
+        }
+    }
+    
     return EXIT_SUCCESS;
 }
